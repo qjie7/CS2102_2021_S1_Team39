737413403df40eb3500097133bcd920f468b6914
Qian Jie <qianjie777@gmail.com>
2020-11-14 02:05:54 +0800

    another fix3 to heroku deploy

--- App/passportConfig_admin.js
+++ App/passportConfig_admin.js
@@ -1,71 +1,71 @@
-const LocalStrategy = require("passport-local").Strategy;
-const { pool } = require("./dbConfig");
-const bcrypt = require("bcrypt");
+// const LocalStrategy = require("passport-local").Strategy;
+// const { pool } = require("./dbConfig");
+// const bcrypt = require("bcrypt");
 
-function initialize(passport) {
-  const authenticateUser = (email, password, done) => {
-    console.log(email, password);
-    pool.query(
-      `SELECT * FROM admin WHERE email = $1`,
-      [email],
-      (err, results) => {
-        if (err) {
-          throw err;
-        }
-        console.log(results.rows);
+// function initialize(passport) {
+//   const authenticateUser = (email, password, done) => {
+//     console.log(email, password);
+//     pool.query(
+//       `SELECT * FROM admin WHERE email = $1`,
+//       [email],
+//       (err, results) => {
+//         if (err) {
+//           throw err;
+//         }
+//         console.log(results.rows);
 
-        if (results.rows.length > 0) {
-          const user = results.rows[0];
+//         if (results.rows.length > 0) {
+//           const user = results.rows[0];
 
-          bcrypt.compare(password, user.password, (err, isMatch) => {
-            if (err) {
-              throw err;
-            }
-            if (isMatch) {
-              return done(null, user);
-            } else {
-              //password is incorrect
-              return done(null, false, { message: "Password is incorrect" });
-            }
-          });
-        } else {
-          // No user
-          return done(null, false, {
-            message: "No user with that email address",
-          });
-        }
-      }
-    );
-  };
+//           bcrypt.compare(password, user.password, (err, isMatch) => {
+//             if (err) {
+//               throw err;
+//             }
+//             if (isMatch) {
+//               return done(null, user);
+//             } else {
+//               //password is incorrect
+//               return done(null, false, { message: "Password is incorrect" });
+//             }
+//           });
+//         } else {
+//           // No user
+//           return done(null, false, {
+//             message: "No user with that email address",
+//           });
+//         }
+//       }
+//     );
+//   };
 
-  passport.use(
-    new LocalStrategy(
-      { usernameField: "email", passwordField: "password" },
-      authenticateUser
-    )
-  );
-  // Stores user details inside session. serializeUser determines which data of the user
-  // object should be stored in the session. The result of the serializeUser method is attached
-  // to the session as req.session.passport.user = {}. Here for instance, it would be (as we provide
-  //   the user id as the key) req.session.passport.user = {id: 'xyz'}
-  passport.serializeUser((user, done) => done(null, user.email));
+//   passport.use(
+//     new LocalStrategy(
+//       { usernameField: "email", passwordField: "password" },
+//       authenticateUser
+//     )
+//   );
+//   // Stores user details inside session. serializeUser determines which data of the user
+//   // object should be stored in the session. The result of the serializeUser method is attached
+//   // to the session as req.session.passport.user = {}. Here for instance, it would be (as we provide
+//   //   the user id as the key) req.session.passport.user = {id: 'xyz'}
+//   passport.serializeUser((user, done) => done(null, user.email));
 
-  // In deserializeUser that key is matched with the in memory array / database or any data resource.
-  // The fetched object is attached to the request object as req.user
+//   // In deserializeUser that key is matched with the in memory array / database or any data resource.
+//   // The fetched object is attached to the request object as req.user
 
-  passport.deserializeUser((email, done) => {
-    pool.query(
-      `SELECT * FROM admin WHERE email = $1`,
-      [email],
-      (err, results) => {
-        if (err) {
-          return done(err);
-        }
-        // console.log(`email is ${results.rows[0].email}`);
-        return done(null, results.rows[0]);
-      }
-    );
-  });
-}
+//   passport.deserializeUser((email, done) => {
+//     pool.query(
+//       `SELECT * FROM admin WHERE email = $1`,
+//       [email],
+//       (err, results) => {
+//         if (err) {
+//           return done(err);
+//         }
+//         // console.log(`email is ${results.rows[0].email}`);
+//         return done(null, results.rows[0]);
+//       }
+//     );
+//   });
+// }
 
-module.exports = initialize;
+// module.exports = initialize;
